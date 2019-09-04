3439f848fd7ca3b8b0368582c1027f157f9e9e44
raisusmita <42989198+raisusmita@users.noreply.github.com>
2019-09-04 15:52:35 +0545

    Updates

---
+++ precache-manifest.8b3b140400f73aa9e8f8a4ddc8238496.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b1e7fcc8b8d27d8030a2c737e8356d",
+    "url": "/todos-react/index.html"
+  },
+  {
+    "revision": "5fbadbf7adb83b41fc71",
+    "url": "/todos-react/static/css/main.eb514cd6.chunk.css"
+  },
+  {
+    "revision": "ebd082a754c464e8ebdb",
+    "url": "/todos-react/static/js/2.f9364419.chunk.js"
+  },
+  {
+    "revision": "5fbadbf7adb83b41fc71",
+    "url": "/todos-react/static/js/main.fdcb169d.chunk.js"
+  },
+  {
+    "revision": "fcc42c84de1085886446",
+    "url": "/todos-react/static/js/runtime~main.3670f1b9.js"
+  }
+]);
